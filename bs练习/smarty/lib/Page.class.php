33557f8aebcdb90d3121736e123c553a3aeffff6
wanzuliangs <6259608@qq.com>
2017-02-28 17:36:11 +0800

    2017.2.28 update

---
+++ bs练习/smarty/lib/Page.class.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		.paging a {padding:2px 4px;margin-left:4px;border:1px solid #ccc;text-decoration: none;color:#333;font-size: 12px;font-family: '微软雅黑';}
+	</style>
+</head>
+<body>
+	
+</body>
+</html>
+<?php 
+	class Page {
+		private $total;
+		private $pagesize;
+		private $page;
+		private $p;
+		private $url;
+
+		public function __construct($total,$pagesize) {
+			$this->total = $total ? $total : 0;
+			$this->pagesize = $pagesize ? $pagesize : 5;	
+			$this->page = ceil($this->total / $this->pagesize);
+			$this->setCurrentPage();
+			$this->setCurrentUrl();
+		}
+
+		public function setCurrentPage() {
+			$p = !empty($_REQUEST["p"]) ? $_REQUEST["p"] : 1;
+			if (!empty($p)) {
+				if ($p > 0) {
+					if ($p > $this->page) {
+						$this->p = $this->page;
+					} else {
+						$this->p = $p;
+					}
+				} else {
+					$this->p = 1;
+				} 
+			} else {
+				$this->p = 1;
+			}
+		}
+
+		public function setCurrentUrl() {
+			$page_url = $_SERVER["REQUEST_URI"];
+			$url_parse = parse_url($page_url);
+			$new_url = "";
+			if (isset($url_parse["query"])) {
+				$args = array();
+				parse_str($url_parse["query"],$args);
+				if (isset($args['p'])) {
+					unset($args['p']);
+				}
+				$new_url = $url_parse["path"] . "?" . http_build_query($args) . "&";
+			} else {
+				$new_url = $url_parse["path"] . "?";
+			}
+			
+			$this->url = $new_url;
+		}
+
+		public function build_page_list() {
+			$str = '';
+			$str .= '<div class="paging">';
+			$str .= $this->firstPage();
+			$str .= $this->prePage();
+			for ($i = 1; $i <= $this->page; $i++) {
+				$str .= '<a href="' . $this->url . 'p=' . $i . '">' . $i . '</a>';
+			}
+			$str .= $this->nextPage();
+			$str .= $this->lastPage();
+			$str .= '</div>';
+			echo $str;
+		}
+
+		public function firstPage() {
+			return '<a href="' . $this->url . 'p=1">首页</a>';
+		}
+
+		public function lastPage() {
+			return '<a href="' . $this->url . 'p=' . $this->page . '">尾页</a>';
+		}
+
+		public function prePage() {
+			if ($this->p > 1) {
+				return '<a href="' . $this->url . 'p=' . ($this->p - 1) . '">上一页</a>';
+			} else {
+				return '<a href="javascript:;">上一页</a>';	
+			}
+		}
+
+		public function nextPage() {
+			if ($this->p < $this->page) {
+				return '<a href="' . $this->url . 'p=' . ($this->p + 1) . '">下一页</a>';
+			} else {
+				return '<a href="javascript:;">下一页</a>';	
+			}
+		}
+
+
+	}
+	$page = new Page(40,4);
+	$page->build_page_list();
+	echo "<pre>";
+	print_r($page);
+	echo "</pre>";
+?>
